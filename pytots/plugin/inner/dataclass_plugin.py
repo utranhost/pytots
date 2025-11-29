e77219fe3a255379fcfa35c06ba3bd2087c270f2
utran <utran.host@qq.com>
2025-11-29 15:54:53 +0800

    去除冗余实现

--- pytots/plugin/inner/dataclass_plugin.py
+++ pytots/plugin/inner/dataclass_plugin.py
@@ -5,13 +5,8 @@ dataclass类处理插件
 import typing
 from dataclasses import is_dataclass
 import inspect
-from typing import (
-    get_type_hints,
-    get_origin,
-    get_args,
-    Final,
-    ClassVar
-)
+from typing import get_type_hints
+
 
 from .. import Plugin
 from ..tools import generic_feild_fill,assemble_interface_type
@@ -51,15 +46,9 @@ class DataclassPlugin(Plugin):
         """
         class_name = python_type.__name__
         
-        type_hints = get_type_hints(python_type)
         fields = []
-        for field, field_type in type_hints.items():
-            # 通过检查 __origin__ 属性来检测 Final 和 ClassVar
-            if get_origin(field_type) in [Final, ClassVar]:
-                field_type = get_args(field_type)[0]  # 获取 Final 或 ClassVar 的原始类型
-            field_result = generic_feild_fill(self,field_type)
-            ts_type = field_result["code"] if isinstance(field_result, dict) and "code" in field_result else field_result
-
+        for field, field_type in get_type_hints(python_type).items():
+            ts_type = generic_feild_fill(self,field_type)
             if field_type.__dict__.get("_name") == "Optional":
                 fields.append(f"{field}?: {ts_type};")
             else:
